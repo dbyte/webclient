18aa74f1835a42469d64d4b18e004171afff48d7
T. Fornalik <41309806+dbyte@users.noreply.github.com>
2020-12-08 20:23:49 +0100

    feat: Improved SRP in GoogleGeocodingResponseMapper

--- src/main/java/de/fornalik/webclient/webclient/GoogleGeocodingResponseMapper.java
+++ src/main/java/de/fornalik/webclient/webclient/GoogleGeocodingResponseMapper.java
@@ -26,26 +26,11 @@ public class GoogleGeocodingResponseMapper implements Converter<String, Mono<Geo
 
     try {
       dto = mapper.readValue(Objects.requireNonNull(json), Dto.class);
+      return Mono.just(dto.getAsGeo());
     }
-    catch (JsonProcessingException ex) {
+    catch (JsonProcessingException | IllegalStateException ex) {
       return Mono.error(new RuntimeException(ex.getMessage()));
     }
-
-    if (!"OK".equals(dto.status)) {
-      return Mono.error(new IllegalStateException(
-          "REST service response signaled error: " + dto.message));
-    }
-
-    if (dto.results.isEmpty()) {
-      return Mono.error(new IllegalStateException("No geo results in JSON reponse."));
-    }
-
-    if (dto.results.get(0).geometryDto == null
-        || dto.results.get(0).geometryDto.locationDto == null) {
-      return Mono.error(new IllegalStateException("No location data in JSON reponse."));
-    }
-
-    return Mono.just(dto.results.get(0).getAsGeo());
   }
 
   /**
@@ -56,6 +41,29 @@ public class GoogleGeocodingResponseMapper implements Converter<String, Mono<Geo
     @JsonProperty("results") private final ArrayList<ResultDto> results = new ArrayList<>();
     @JsonProperty("status") private String status;
     @JsonProperty("error_message") private String message;
+
+    private Geo getAsGeo() {
+      validateStructure();
+
+      return Geo.of(
+          results.get(0).geometryDto.locationDto.latitude,
+          results.get(0).geometryDto.locationDto.longitude);
+    }
+
+    private void validateStructure() {
+      if (!"OK".equals(status)) {
+        throw new IllegalStateException("REST service response signaled error: " + message);
+      }
+
+      if (results.isEmpty()) {
+        throw new IllegalStateException("No geo results in JSON reponse.");
+      }
+
+      if (results.get(0).geometryDto == null
+          || results.get(0).geometryDto.locationDto == null) {
+        throw new IllegalStateException("No geo location data in JSON reponse.");
+      }
+    }
   }
 
   /**
@@ -64,27 +72,23 @@ public class GoogleGeocodingResponseMapper implements Converter<String, Mono<Geo
    */
   private static class ResultDto {
     @JsonProperty("geometry") private GeometryDto geometryDto;
+  }
 
-    private Geo getAsGeo() {
-      return Geo.of(geometryDto.locationDto.latitude, geometryDto.locationDto.longitude);
-    }
+  /**
+   * Class provides object relational mapping support for JSON. It represents the "geometry"
+   * json object within one element of json array "results" of the Google Geocoding response.
+   */
+  private static class GeometryDto {
+    @JsonProperty("location") private LocationDto locationDto;
+    @JsonProperty("location_type") private String locationType;
+  }
 
-    /**
-     * Class provides object relational mapping support for JSON. It represents the "geometry"
-     * json object within one element of json array "results" of the Google Geocoding response.
-     */
-    private static class GeometryDto {
-      @JsonProperty("location") private LocationDto locationDto;
-      @JsonProperty("location_type") private String locationType;
-    }
-
-    /**
-     * Class provides object relational mapping support for JSON. It represents the "location"
-     * json object within the json object "geometry" of the Google Geocoding response.
-     */
-    private static class LocationDto {
-      @JsonProperty("lat") private Double latitude;
-      @JsonProperty("lng") private Double longitude;
-    }
+  /**
+   * Class provides object relational mapping support for JSON. It represents the "location"
+   * json object within the json object "geometry" of the Google Geocoding response.
+   */
+  private static class LocationDto {
+    @JsonProperty("lat") private Double latitude;
+    @JsonProperty("lng") private Double longitude;
   }
 }
