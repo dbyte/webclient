0e8f54df4205a34f45d72abc884c9e5b694ea5aa
T. Fornalik <41309806+dbyte@users.noreply.github.com>
2020-12-09 19:54:30 +0100

    fix: Remove unused member

--- src/main/java/de/fornalik/webclient/application/webclient/WebClientConfiguration.java
+++ src/main/java/de/fornalik/webclient/application/webclient/WebClientConfiguration.java
@@ -1,10 +1,8 @@
 package de.fornalik.webclient.application.webclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -12,8 +10,6 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 class WebClientConfiguration implements WebFluxConfigurer {
-
-  @Autowired private Jackson2ObjectMapperBuilder objectMapperBuilder;
 
   @Bean
   @Scope("prototype")
